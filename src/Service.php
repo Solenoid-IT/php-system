b56357918498988e226cc74aacf86b3714daacff
CapitanPNG <pierniccologiannelli@gmail.com>
2024-06-30 00:40:09 +0000

    service

---
+++ src/Service.php
@@ -0,0 +1,116 @@
+<?php
+
+
+
+namespace Solenoid\System;
+
+
+
+class Service
+{
+    protected int  $refresh_interval;
+
+    protected bool $running;
+    protected      $handler;
+
+
+
+    # Returns [self]
+    private function signal_handler ($signal)
+    {
+        switch ($signal)
+        {
+            case SIGCHLD:
+                // (Waiting for the child process)
+                $pid = pcntl_waitpid( -1, $status, WNOHANG );
+
+                while ( $pid > 0 )
+                {// Processing each clock
+                    // (Getting the value)
+                    $exit_code = pcntl_wexitstatus($status);
+
+                    // (Getting the value)
+                    $pid = pcntl_waitpid( -1, $status, WNOHANG );
+                }
+            break;
+
+            default:
+                // (Calling the function)
+                ( $this->handler )($signal);
+
+
+
+                // (Setting the value)
+                $this->running = false;
+        }
+
+
+
+        // Returning the value
+        return $this;
+    }
+
+
+
+    # Returns [self]
+    public function __construct (int $refresh_interval = 1)
+    {
+        // (Getting the value)
+        $this->refresh_interval = $refresh_interval;
+    }
+
+
+
+    # Returns [self]
+    public function handle_signal (callable $handler)
+    {
+        // (Getting the value)
+        $this->handler = $handler;
+
+
+
+        // Returning the value
+        return $this;
+    }
+
+    # Returns [self]
+    public function run (callable $function)
+    {
+        // (Registering the handlers)
+        pcntl_signal( SIGTERM, [ $this, 'signal_handler' ] );
+        pcntl_signal( SIGHUP, [ $this, 'signal_handler' ] );
+        pcntl_signal( SIGINT, [ $this, 'signal_handler' ] );
+        pcntl_signal( SIGCHLD, [ $this, 'signal_handler' ] );
+
+
+
+        // (Setting the value)
+        $this->running = true;
+
+
+
+        // (Declaring the variable)
+        declare ( ticks = 1 )
+        {
+            while ( $this->running )
+            {
+                // (Calling the function)
+                $function();
+
+
+
+                // (Waiting for the time)
+                sleep( $this->refresh_interval );
+            }
+        }
+
+
+
+        // Returning the value
+        return $this;
+    }
+}
+
+
+
+?>
